4b6afe97b992cb3306d66dd4896808220c209a51
alexlobko <92lobko@gmail.com>
2024-01-04 23:18:09 +0300

    feat: add feature with answer's values in  mistery.py

--- HW_mystery/mistery.py
+++ HW_mystery/mistery.py
@@ -4,22 +4,26 @@ import random
 class Mistery:
     def __init__(self, question, answer, choices):
         self.question = question
-        self.answer = answer
+        self.answer = {answer: 3}
         self.choices = choices
+
 
     def quiz(self):
         print(self.question)
-        random.shuffle(self.choices)
-        answers = [self.answer] + self.choices[:3]
+        answers = list(self.answer) + list(set(self.choices.keys()))[:3]
         random.shuffle(answers)
         print(*[f'{i}. {j}' for i, j in enumerate(answers)], sep='\n')
         user_choice = input('choose answer: ')
-        if answers[int(user_choice)] == self.answer:
-            print(True)
-            return True
+        if user_choice == '' or not user_choice.isdigit() or int(user_choice) not in range(len(answers)):
+            print('You got 0 points')
+            return 0
+        elif answers[int(user_choice)] in self.answer:
+            print('Right answer! You got 3 points')
+            return 3
         else:
-            print('wrong answer')
-            return False
+            points = self.choices[answers[int(user_choice)]]
+            print(f'Wrong answer! You got {points} points')
+            return points
 
-q1 = Mistery(question='2x2=', answer='4', choices=['1', '2', '3', '5', '6'])
+q1 = Mistery(question='2x2=', answer='4', choices={'1': -3, '2': -1, '3': 2, '5': 2, '6': -1, '7': -3})
 q1.quiz()
