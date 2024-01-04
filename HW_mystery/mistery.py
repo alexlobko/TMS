68a69216713d48188c86ea89f797d165101e8af5
alexlobko <92lobko@gmail.com>
2024-01-04 23:27:07 +0300

    doc: add docstrings in  mistery.py

--- HW_mystery/mistery.py
+++ HW_mystery/mistery.py
@@ -2,13 +2,29 @@ import random
 
 
 class Mistery:
+    """
+     Class representing a mystery quiz with a question, answer, and choices.
+     """
     def __init__(self, question, answer, choices):
+        """
+        Initialize the Mistery class.
+
+        Args:
+            question (str): The question for the quiz.
+            answer (str): The correct answer to the question.
+            choices (dict): A dictionary of answer choices with their respective point values.
+        """
         self.question = question
         self.answer = {answer: 3}
         self.choices = choices
 
-
     def quiz(self):
+        """
+        Conduct the quiz.
+
+        Returns:
+            int: The number of points earned by the user.
+        """
         print(self.question)
         answers = list(self.answer) + list(set(self.choices.keys()))[:3]
         random.shuffle(answers)
@@ -24,6 +40,7 @@ class Mistery:
             points = self.choices[answers[int(user_choice)]]
             print(f'Wrong answer! You got {points} points')
             return points
+
 
 q1 = Mistery(question='2x2=', answer='4', choices={'1': -3, '2': -1, '3': 2, '5': 2, '6': -1, '7': -3})
 q1.quiz()
